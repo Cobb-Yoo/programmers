a12ae51149f176a9b7ed43501151d5e9d0d2ecee
YooCobb <yhj010597@naver.com>
2020-03-16 03:53:27 +0900

    programmers level 1 서울에서 김서방 찾기

---
+++ l1/서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    string answer = "김서방은 ";
+    int len = seoul.size();
+    
+    for(int i=0;i<len;i++){
+        if(seoul[i] == "Kim") {
+            answer += to_string(i);
+            break;
+        }
+    }
+    return answer+="에 있다";
+}
