82c49af467f9b5780d29955717c2dd2ce9aa0982
YooCobb <yhj010597@naver.com>
2020-03-17 02:38:18 +0900

    programmers level 1 평균구하기

---
+++ l1/평균구하기/평균구하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    int len = arr.size();
+    double answer = 0;
+    for(int i=0;i<len;i++) answer += arr[i];
+    answer /= len;
+    return answer;
+}
