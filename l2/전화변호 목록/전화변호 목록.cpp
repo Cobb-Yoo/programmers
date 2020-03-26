2d0e348f82c71d4c30682d523b652edaab7d84fb
YooCobb <yhj010597@naver.com>
2020-03-27 08:32:28 +0900

    programmers level 2 전화번호 목록 (더 나은 코드)

--- l2/전화변호 목록/전화변호 목록.cpp
+++ l2/전화변호 목록/전화변호 목록.cpp
@@ -3,21 +3,19 @@
 #include <algorithm>
 using namespace std;
 
-bool cmp(string a, string b){
-    return a.size() < b.size();
-}
-
-bool solution(vector<string> phone_book) {
+bool solution(vector<string> phoneBook) {
     bool answer = true;
-    int len = phone_book.size();
-    
-    sort(phone_book.begin(),phone_book.end(),cmp);
-    
-    for(int i=0;i<len-1;i++){
-        for(int j=i+1;j<len;j++){
-            string cut_string = phone_book[j].substr(0,phone_book[i].size());
-            if(phone_book[i] == cut_string) return false;
+
+    sort(phoneBook.begin(), phoneBook.end());
+
+    for ( int i = 0 ; i < phoneBook.size() - 1 ; i++ )
+    {
+        if ( phoneBook[i] == phoneBook[i+1].substr(0, phoneBook[i].size()) )
+        {
+            answer = false;
+            break;
         }
     }
+
     return answer;
 }
