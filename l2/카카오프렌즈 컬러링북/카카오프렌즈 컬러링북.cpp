f59ae7f270c7a57cbc44e3393299dd4f33dbbe71
YooCobb <yhj010597@naver.com>
2020-04-03 12:15:18 +0900

    programmers level 2 카카오프렌즈 컬러링북

--- l2/카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.cpp
+++ l2/카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <cstring>
-#include <iostream>
 using namespace std;
 
 int option[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
@@ -9,14 +8,17 @@ int M,N;
 
 int check(int a,int b,vector<vector<int>> picture){
     int cnt=0;
-    if(0 <= a && a <M-1 && 0 <= b && b < N-1) {
-        for(int i=0;i<4;i++){
-            if(visited[a+option[i][0]][b+option[i][1]]) continue;
-            if(picture[a][b] == picture[a+option[i][0]][b+option[i][1]]){
-                visited[a+option[i][0]][b+option[i][1]] = 1;
-                cnt += check(a+option[i][0],b+option[i][1],picture);
+
+    for(int i=0;i<4;i++){
+        int x = a + option[i][0];
+        int y = b + option[i][1];
+        if(0 <= x && x < M && 0 <= y && y < N){
+            if(!visited[x][y] && picture[a][b] == picture[x][y]){
+                visited[x][y] = 1;
+                cnt++;
+                cnt += check(x,y,picture);
             }
-        }    
+        }
     }
     return cnt;
 }
@@ -24,44 +26,25 @@ int check(int a,int b,vector<vector<int>> picture){
 vector<int> solution(int m, int n, vector<vector<int>> picture) {
     M = m;
     N = n;
-    
+
     memset(visited,0,sizeof(visited));
-    
+
     int number_of_area = 0;
     int max_size_of_one_area = 0;
-    
+
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
-            if(visited[i][j]) cout << 1;
-            else cout << 0;
-        }
-        cout << endl;
-    }
-    cout << "=================" << endl;
-    
-    
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(visited[i][j]) continue;
-            if(!picture[i][j]) continue;
-            
+            if(visited[i][j] || !picture[i][j]) continue;
+
             int tmp = check(i,j,picture);
-            
+
+            number_of_area++;
             if(tmp > max_size_of_one_area) {
-                number_of_area++;
                 max_size_of_one_area = tmp;
             }
         }
     }
-    
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(visited[i][j]) cout << 1;
-            else cout << 0;
-        }
-        cout << endl;
-    }
-    
+
     vector<int> answer(2);
     answer[0] = number_of_area;
     answer[1] = max_size_of_one_area;
