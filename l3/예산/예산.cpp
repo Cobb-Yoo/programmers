df2d592880e6fb47a82e247e4948a0afc7bb3fee
YooCobb <yhj010597@naver.com>
2020-04-07 18:11:46 +0900

    programmers level 3 예산(더 좋은 코드)

--- l3/예산/예산.cpp
+++ l3/예산/예산.cpp
@@ -1,32 +1,22 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
 int solution(vector<int> budgets, int M) {
     int answer = 0;
+    int numbers = budgets.size();
+
     sort(budgets.begin(),budgets.end());
-    int len = budgets.size();
-    
-    int max = budgets[len-1];
-    int min = 0;
-    int mid;
-    int tmp = 0;
-    
-    while(min <= max){
-        mid = (max + min)/2;
-        
-        int result = 0;
-        for(int i=0;i<len;i++){
-            if(mid > budgets[i]) result += budgets[i];
-            else result += mid;
-        }
-        
-        if(result > M) max = mid - 1;
+
+    for(auto itr=budgets.begin(); itr!= budgets.end(); itr++){
+        if(*itr > (M / numbers)) return M/numbers;
         else{
-            if(result >= tmp) answer = mid;
-            min = mid + 1;
+            M -= *itr;
+            numbers--;
         }
     }
-    return answer;
+
+    return budgets.back();
 }
